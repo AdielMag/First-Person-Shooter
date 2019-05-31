6cbad857db7bc9e99715d0757aeda17a2d298c81
AdielMag <Adiel12430@gmail.com>
2019-05-31 13:34:38 +0300

    Laset Update
    New script to determine laser colliison and light at the end.

---
+++ FPS/Assets/Scripts/LaserAttachment.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserAttachment : MonoBehaviour
+{
+
+    public GameObject collisionLight;
+    public Transform origin;
+    LineRenderer lineRen;
+
+    Ray ray;
+    RaycastHit hit;
+
+    private void Start()
+    {
+        origin = transform.GetChild(1);
+        lineRen = transform.GetChild(1).GetComponent<LineRenderer>();
+    }
+
+    private void Update()
+    {
+        ray = new Ray(origin.position, origin.forward);
+        if (Physics.Raycast(ray, out hit,6)) 
+        {
+            lineRen.SetPosition(1, new Vector3(0, 0, hit.distance));
+            collisionLight.transform.position = origin.position + origin.forward * hit.distance;
+        }
+        else 
+        {
+            lineRen.SetPosition(1, new Vector3(0, 0, 6));
+            collisionLight.transform.position = origin.position + origin.forward * 6;
+        }
+    }
+}
